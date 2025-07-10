d8198f9b1f4ddebb51328b35d1eab33f9a10c5ee
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-07-10 09:19:21 +0900

    bundle gem fast_schema_dumper

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in fast_schema_dumper.gemspec
+gemspec
+
+gem "irb"
+gem "rake", "~> 13.0"
---
+++ lib/fast_schema_dumper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fast_schema_dumper/version"
+
+module FastSchemaDumper
+  class Error < StandardError; end
+  # Your code goes here...
+end
